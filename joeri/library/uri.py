c1bd78b040a775f71cd0c4611e39ff431b03e070
Serge van Ginderachter <serge@vanginderachter.be>
2019-03-07 07:12:17 +0100

    uri.py needs python3

--- joeri/library/uri.py
+++ joeri/library/uri.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#! /usr/bin/python3
 
-print '{"changed": false, "failed": false, "msg": "custom module run"}'
+print ('{"changed": false, "failed": false, "msg": "custom module run"}')
 
